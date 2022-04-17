a790ebc319c26f005d0aa4fcce030b79d9b0aa92
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-04-17 16:59:48 -0400

    Please work

--- solution.py
+++ solution.py
@@ -112,7 +112,8 @@ def get_route(hostname):
                 #Fill in end
                 try: #try to fetch the hostname
                     #Fill in start
-                    tracelist1.append(gethostbyaddr(str(addr[0]))[0])
+                    #tracelist1.append(gethostbyaddr(str(addr[0]))[0])
+                    tracelist1.append(gethostbyaddr)
                     #Fill in end
                 except herror:   #if the host does not provide a hostname
                     #Fill in start
