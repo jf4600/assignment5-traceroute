888d8baf667d54fb80e92caabbd06348c9c89eb1
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-04-17 19:03:47 -0400

    Please work

--- solution.py
+++ solution.py
@@ -121,8 +121,7 @@ def get_route(hostname):
 
                 if types == 11:
                     bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recPacket[28:28 +
-                    bytes])[0]
+                    timeSent = struct.unpack("d", recPacket[28:28 + bytes])[0]
                     #Fill in start
                     tracelist1.insert(-1, str(int((timeReceived - t) * 1000)) + "ms")
                     tracelist1.insert(-1, addr[0])
@@ -146,7 +145,7 @@ def get_route(hostname):
                     tracelist1.insert(-1, addr[0])
                     tracelist2.append(tracelist1)
                     #You should add your responses to your lists here and return your list if your destination IP is met
-                    return tracelist2
+                    print(tracelist2)
                     #Fill in end
                 else:
                     #Fill in start
